8dba5283bfb1b6d127c22f1bfdc6645a875509a2
mdb <dauren.asir@gmail.com>
2023-07-28 18:06:30 +0600

    fix grpc

--- adapters/server/grpc/index.go
+++ adapters/server/grpc/index.go
@@ -4,43 +4,35 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/mechta-market/dop/adapters/logger"
 	"google.golang.org/grpc"
 )
 
 type St struct {
-	lg logger.Lite
-
 	Server *grpc.Server
 	eChan  chan error
 }
 
-func New(lg logger.Lite, opts ...grpc.ServerOption) *St {
+func New(opts ...grpc.ServerOption) *St {
 	return &St{
-		lg: lg,
-
 		Server: grpc.NewServer(opts...),
 		eChan:  make(chan error, 1),
 	}
 }
 
-func (s *St) Start(port string) error {
+func (s *St) Start(port string) (string, error) {
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		return fmt.Errorf("fail to listen: %w", err)
+		return "", fmt.Errorf("fail to listen: %w", err)
 	}
-
-	s.lg.Infow("Start grpc server", "addr", lis.Addr().String())
 
 	go func() {
 		err = s.Server.Serve(lis)
 		if err != nil {
-			s.lg.Errorw("GRPC server closed", err)
 			s.eChan <- err
 		}
 	}()
 
-	return nil
+	return lis.Addr().String(), nil
 }
 
 func (s *St) Wait() <-chan error {
